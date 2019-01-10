a70ecec75b05406e4f0728aa70e6a574ec9f707d
14Praveen08 <40239567+14Praveen08@users.noreply.github.com>
2019-01-10 18:03:18 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,8 +1,9 @@
+# your code goes here
 a,b = map(int,input().split(" "))
 for i in range(a+1,b):
-	if i==b:
+	if i==b-1:
 		if i%2!=0:
-			print(i)
+			print(i,end="")
 	else:
 		if i%2!=0:
 			print(i,end=" ")
