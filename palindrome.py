9c29fbe9d27512b2b861180fba1bea73afed1d59
14Praveen08 <40239567+14Praveen08@users.noreply.github.com>
2019-01-10 17:36:00 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+number=int(input())
+temporary=number
+reverse=0
+while(number>0):
+    digit=number%10
+    reverse=reverse*10+digit
+    number=number//10
+if(temporary==reverse):
+    print("Yes")
+else:
+    print("No")
