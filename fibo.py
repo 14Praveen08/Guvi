a3d0379094a6b787d9f987ba9803d81397da1e0c
Praveen Kumaar <actionking148@gmail.com>
2019-01-11 01:02:06 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -2,12 +2,15 @@ number = int(input())
 frst = 0
 temp = 0
 second = 1
+count = 1
 print(second,end=" ")
-for i in range(0,number):
+while count < number:
 	temp = frst + second
 	frst = second
 	second = temp
-	if i == number-1:
+	count = count + 1
+	if count == number:
 		print(temp,end="")
 	else:
 		print(temp,end=" ")
+	
