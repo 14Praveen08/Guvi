6af8d06536cb5be0a854b3bf767eb0e289f753c0
14Praveen08 <40239567+14Praveen08@users.noreply.github.com>
2019-01-10 17:03:25 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum=0;
+	int arr[a];
+	cin>>a>>b;
+	for(int i=0;i<a;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=0;i<b;i++)
+	{
+		sum=sum+arr[i];
+	}
+	cout<<sum;
+return 0;
+	
+}
