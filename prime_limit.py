471dfbecb2ebbd2195805d73a1206cf200fa7932
Praveen Kumaar <actionking148@gmail.com>
2019-01-11 00:04:02 +0530

    Update prime_limit.py

--- prime_limit.py
+++ prime_limit.py
@@ -1,12 +1,16 @@
 # your code goes here
-a,b=map(int,input().split(" "))
+a,b = map(int,input().split(" "))
+ans = []
 for num in range(a,b+1):
 	if num > 1:
 		for i in range(2,num):
 			if (num % i) == 0:
 				break
 		else:
-			if num == b:
-				print(num,end="")
-			else:
-				print(num,end=" ")
+			ans.append[num]
+			
+for i in range(len(ans)):
+	if i == len(ans)-1:
+		print(ans[i],end="")
+	else:
+		print(ans[i],end=" ")
