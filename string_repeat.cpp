be9ab7563fef8b97c5a609759a3d3b51eaf11229
Praveen Kumaar <actionking148@gmail.com>
2019-01-11 15:41:03 +0530

    Update string_repeat.cpp

--- string_repeat.cpp
+++ string_repeat.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
 	// your code goes here
-	string a;
-	int num,i=0;
-	cin>>a>>num;
+	string string;
+	int num,i;
+	cin>>string>>num;
 	for(i=0;i<num;i++){
-		cout<<a<<"\n";
+		cout<<string<<"\n";
 	}
 	return 0;
 }
