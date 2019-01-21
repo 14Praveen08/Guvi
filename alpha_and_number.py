f90b28af2d755501d87a6287121801c07d1cdfce
Praveen Kumaar <actionking148@gmail.com>
2019-01-21 16:26:43 +0530

    Update alpha_and_number.py

--- alpha_and_number.py
+++ alpha_and_number.py
@@ -1,5 +1,14 @@
 string = input()
-if(string.isalnum() == False):
-	print("No")
-else:
+a=0
+f=0
+for i in string:
+	if(i.isnumeric()):
+		a = a + 1
+	elif(i.isalpha()):
+		f = f + 1
+	else:
+		continue
+if(a>0 and f>0):
 	print("Yes")
+else:
+	print("No")
