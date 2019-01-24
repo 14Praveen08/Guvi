9bc17f3c1ea61aa65cb878e07fe635543e496d6f
Praveen Kumaar <actionking148@gmail.com>
2019-01-24 18:55:14 +0530

    Create GUD_of_2.py

---
+++ GUD_of_2.py
@@ -0,0 +1,9 @@
+num1,num2 = map(int,input().split(" "))
+if num1>num2:
+   small = num2
+else:
+   small = num1
+for i in range(1,small+1):
+    if((num1 % i == 0) and (num2 % i == 0)):
+            hcf = i
+print(hcf)
