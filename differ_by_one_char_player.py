536f495ad2e23f03ab18c3158bd04f3d5297c613
Praveen Kumaar <actionking148@gmail.com>
2019-01-29 02:35:14 -0800

    Create differ_by_one_char_player.py

---
+++ differ_by_one_char_player.py
@@ -0,0 +1,20 @@
+str1,str2 = map(str,input().split(" "))
+list1=[]
+list2=[]
+count = 0
+if len(str1) != len(str2):
+	print("no")
+else:
+	for i in str1:
+		list1.append(i)
+	for i in str2:
+		list2.append(i)
+	for i in range(0,len(list1)):
+		if list1[i] == list2[i]:
+			()
+		else:
+			count=count+1
+	if count>0:
+		print("no")
+	else:
+		print("yes")
