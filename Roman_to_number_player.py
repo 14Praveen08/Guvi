c264620be6dbc66ba6388d6471dfc50210ce398b
Praveen Kumaar <actionking148@gmail.com>
2019-01-29 02:12:37 -0800

    Create Roman_to_number_player.py

---
+++ Roman_to_number_player.py
@@ -0,0 +1,45 @@
+# your code goes here
+string = input()
+string = string.upper()
+if string == 'I':
+	print('1')
+elif string == 'II':
+	print('2')
+elif string == 'III':
+	print('3')
+elif string == 'IV':
+	print('4')
+elif string == 'V':
+	print('5')
+elif string == 'VI':
+	print('6')
+elif string == 'VII':
+	print('7')
+elif string == 'VIII':
+	print('8')
+elif string == 'IX':
+	print('9')
+elif string == 'X':
+	print('10')
+elif string == 'XI':
+	print('11')
+elif string == 'XII':
+	print('12')
+elif string == 'XIII':
+	print('13')
+elif string == 'XIV':
+	print('14')
+elif string == 'XV':
+	print('15')
+elif string == 'XVI':
+	print('16')
+elif string == 'XVII':
+	print('17')
+elif string == 'XVIII':
+	print('18')
+elif string == 'XIX':
+	print('19')
+elif string == 'XX':
+	print('20')
+else:
+	()
