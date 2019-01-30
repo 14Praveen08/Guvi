273e75cd3cd78019a34e43fdf9182bd8c4bc0e0b
Praveen Kumaar <actionking148@gmail.com>
2019-01-30 01:55:17 -0800

    Create count_prime_player.py

---
+++ count_prime_player.py
@@ -0,0 +1,10 @@
+a,b = map(int,input().split(" "))
+count = 0
+for num in range(a,b+1):
+	if num > 1:
+		for i in range(2,num):
+			if (num % i) == 0:
+				break
+		else:
+			count = count + 1
+print(count)
