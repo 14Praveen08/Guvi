040eb5dea05849240eeeba7e3d09adfa26c3e081
Praveen Kumaar <actionking148@gmail.com>
2019-01-30 02:38:59 -0800

    Create reverse_list_after_removing_vowels.py

---
+++ reverse_list_after_removing_vowels.py
@@ -0,0 +1,9 @@
+string = input()
+list1=[]
+for i in string:
+	if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U':
+		continue
+	else:
+		list1.append(i)
+list1.reverse()
+print(''.join(map(str,list1)))
