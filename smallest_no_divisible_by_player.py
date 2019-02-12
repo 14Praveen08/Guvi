c5602e6bc87050d7bbbd7af8716f94adfaee36e3
Praveen Kumaar <actionking148@gmail.com>
2019-02-12 02:11:45 -0800

    Update smallest_no_divisible_by_player.py

--- smallest_no_divisible_by_player.py
+++ smallest_no_divisible_by_player.py
@@ -1,2 +1,11 @@
-num1,num2 =  map(int,input().split(" "))
-print(num1*num2)
+num,num1 = map(int,input().split(" "))
+if num > num1:
+	greater = num
+else:
+	greater = num1
+while(True):
+	if((greater % num == 0) and (greater % num1 == 0)):
+		lcm = greater
+		break
+	greater = greater + 1
+print(lcm)
